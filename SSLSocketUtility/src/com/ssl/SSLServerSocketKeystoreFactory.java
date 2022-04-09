bb04e89bb6400a3ca4c3ef2ec439b04cdedfeac1
William <wiliamwijaya1985@gmail.com>
2022-04-09 14:28:51 +0700

    style: remove unneded line and improve style

--- SSLSocketUtility/src/com/ssl/SSLServerSocketKeystoreFactory.java
+++ SSLSocketUtility/src/com/ssl/SSLServerSocketKeystoreFactory.java
@@ -1,23 +1,5 @@
 package com.ssl;
 
-/*
- *  Copyright (C) 2015 Gabriel POTTER (gpotter2)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -41,37 +23,17 @@ import javax.net.ssl.X509TrustManager;
 
 /**
  * Util class to create SSLServerSocket using a KeyStore certificate to connect a server
- *
- * @author gpotter2
- *
  */
 public class SSLServerSocketKeystoreFactory {
 
-    private static String instance;
-    /**
-     * CONFIGURATION SECTION
-     */
-    static {
-        instance = "JKS"/* TODO REPLACE WITH BKS IF USING IT*/;
-        /*
-         * Several Notes:
-         * - Android only works with BKS, so you need to use only BKS certs files
-         * - As before Android 15, BKS-v1 was used, you need to convert BKS in BKS-v1 to use it in Android 15-; BUT as Android 23+ doesn't support BKS-v1
-         * and as BKS-v1 is deprecated, you need to have both of the certs and use them in fuction of the version
-         * - Java doesn't support BKS without library
-         * - A BKS format client can be connected a JKS format server
-         */
-    }
+    private static String instance = "JKS";
 
     /**
-     *
      * A SSL algorithms types chooser enum
      *
      * @author gpotter2
-     *
      */
     public static enum ServerSecureType {
-//        @Deprecated
         SSL("SSL"),
         @Deprecated
         SSLv2("SSLv2"),
@@ -85,79 +47,87 @@ public class SSLServerSocketKeystoreFactory {
 
         private String type;
 
-        private ServerSecureType(String type){
+        private ServerSecureType(String type) {
             this.type = type;
         }
-        public String getType(){
+
+        public String getType() {
             return type;
         }
     }
 
     /**
-     *
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessource()....)
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessource()....)
      * @param passwordFromCert The password of the KeyStore cert
      * @return The SSLServerSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
-     * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws IOException               If the socket couldn't be created
+     * @throws KeyManagementException    If the KeyManager couldn't be loaded
+     * @throws CertificateException      If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws NoSuchAlgorithmException  If the certificate is from an unknown type
+     * @throws KeyStoreException         If your system is not compatible with JKS KeyStore certificates
      * @throws UnrecoverableKeyException Cannot get the keys of the KeyStore
-     * @author gpotter2
      */
-    public static SSLServerSocket getServerSocketWithCert(int port, String pathToCert, String passwordFromCert, ServerSecureType type) throws IOException,
-        KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, UnrecoverableKeyException{
+    public static SSLServerSocket getServerSocketWithCert(
+            int port, String pathToCert, String passwordFromCert, ServerSecureType type
+    ) throws
+            IOException, KeyManagementException, NoSuchAlgorithmException,
+            CertificateException, KeyStoreException, UnrecoverableKeyException
+    {
         File f = new File(pathToCert);
-        if(!f.exists()){
+        if (!f.exists()) {
             throw new NullPointerException("The specified path point to a non existing file !");
         }
         return getServerSocketWithCert(port, new FileInputStream(f), passwordFromCert, type);
     }
 
     /**
-     *
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
      * @param passwordFromCert The password of the KeyStore cert
      * @return The SSLServerSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
-     * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws IOException               If the socket couldn't be created
+     * @throws KeyManagementException    If the KeyManager couldn't be loaded
+     * @throws CertificateException      If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws NoSuchAlgorithmException  If the certificate is from an unknown type
+     * @throws KeyStoreException         If your system is not compatible with JKS KeyStore certificates
      * @throws UnrecoverableKeyException Cannot get the keys of the KeyStore
-     * @author gpotter2
      */
-    public static SSLServerSocket getServerSocketWithCert(int port, InputStream pathToCert, String passwordFromCert, ServerSecureType type) throws IOException,
-        KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, UnrecoverableKeyException{
+    public static SSLServerSocket getServerSocketWithCert(
+            int port, InputStream pathToCert,
+            String passwordFromCert, ServerSecureType type
+    ) throws IOException, KeyManagementException, NoSuchAlgorithmException,
+            CertificateException, KeyStoreException, UnrecoverableKeyException
+    {
         X509TrustManager[] tmm;
         X509KeyManager[] kmm;
-        KeyStore ks  = KeyStore.getInstance(instance);
+
+        KeyStore ks = KeyStore.getInstance(instance);
         ks.load(pathToCert, passwordFromCert.toCharArray());
-        tmm=tm(ks);
-        kmm=km(ks, passwordFromCert);
+        tmm = getTrustManager(ks);
+        kmm = getKeyManager(ks, passwordFromCert);
+
         SSLContext ctx = SSLContext.getInstance(type.getType());
         ctx.init(kmm, tmm, null);
-        SSLServerSocketFactory socketFactory = (SSLServerSocketFactory) ctx.getServerSocketFactory();
-        SSLServerSocket ssocket = (SSLServerSocket) socketFactory.createServerSocket(port);
-        return ssocket;
+
+        SSLServerSocketFactory socketFactory = ctx.getServerSocketFactory();
+        return (SSLServerSocket) socketFactory.createServerSocket(port);
     }
+
     /**
      * Util class to get the X509TrustManager
-     *
      *
      * @param keystore
      * @return
      * @throws NoSuchAlgorithmException
      * @throws KeyStoreException
-     * @author gpotter2
      */
-    private static X509TrustManager[] tm(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
+    private static X509TrustManager[] getTrustManager(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
         TrustManagerFactory trustMgrFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustMgrFactory.init(keystore);
+
         TrustManager trustManagers[] = trustMgrFactory.getTrustManagers();
+
         for (int i = 0; i < trustManagers.length; i++) {
             if (trustManagers[i] instanceof X509TrustManager) {
                 X509TrustManager[] tr = new X509TrustManager[1];
@@ -166,22 +136,23 @@ public class SSLServerSocketKeystoreFactory {
             }
         }
         return null;
-    };
+    }
+
     /**
      * Util class to get the X509KeyManager
      *
      * @param keystore
      * @param password
      * @return
-     * @throws NoSuchAlgorithmException
-     * @throws KeyStoreException
-     * @throws UnrecoverableKeyException
-     * @author gpotter2
      */
-    private static X509KeyManager[] km(KeyStore keystore, String password) throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException {
+    private static X509KeyManager[] getKeyManager(KeyStore keystore, String password)
+            throws NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException
+    {
         KeyManagerFactory keyMgrFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
         keyMgrFactory.init(keystore, password.toCharArray());
+
         KeyManager keyManagers[] = keyMgrFactory.getKeyManagers();
+
         for (int i = 0; i < keyManagers.length; i++) {
             if (keyManagers[i] instanceof X509KeyManager) {
                 X509KeyManager[] kr = new X509KeyManager[1];
@@ -190,5 +161,5 @@ public class SSLServerSocketKeystoreFactory {
             }
         }
         return null;
-    };
+    }
 }
--- SSLSocketUtility/src/com/ssl/SSLSocketKeystoreFactory.java
+++ SSLSocketUtility/src/com/ssl/SSLSocketKeystoreFactory.java
@@ -1,23 +1,5 @@
 package com.ssl;
 
-/*
- *  Copyright (C) 2015 Gabriel POTTER (gpotter2)
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -41,37 +23,17 @@ import javax.net.ssl.X509TrustManager;
 
 /**
  * Util class to create SSLSocket using a KeyStore certificate to connect a server
- *
- * @author gpotter2
- *
  */
 public class SSLSocketKeystoreFactory {
 
-    private static String instance;
-    /**
-     * CONFIGURATION SECTION
-     */
-    static {
-        instance = "JKS"/* TODO REPLACE WITH BKS IF USING IT*/;
-        /*
-         * Several Notes:
-         * - Android only works with BKS, so you need to use only BKS certs files
-         * - As before Android 15, BKS-v1 was used, you need to convert BKS in BKS-v1 to use it in Android 15-; BUT as Android 23+ doesn't support BKS-v1
-         * and as BKS-v1 is deprecated, you need to have both of the certs and use them in fuction of the version
-         * - Java doesn't support BKS without library
-         * - A BKS format client can be connected a JKS format server
-         */
-    }
+    private static String instance = "JKS";
 
     /**
-     *
      * A SSL algorithms types chooser enum
      *
      * @author gpotter2
-     *
      */
     public static enum SecureType {
-//        @Deprecated
         SSL("SSL"),
         @Deprecated
         SSLv2("SSLv2"),
@@ -85,10 +47,11 @@ public class SSLSocketKeystoreFactory {
 
         private String type;
 
-        private SecureType(String type){
+        private SecureType(String type) {
             this.type = type;
         }
-        public String getType(){
+
+        public String getType() {
             return type;
         }
     }
@@ -96,30 +59,32 @@ public class SSLSocketKeystoreFactory {
     /**
      * Instantiate sslsocket
      *
-     * @param ip The IP to connect the socket to
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessource()....)
+     * @param ip               The IP to connect the socket to
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessource()....)
      * @param passwordFromCert The password of the KeyStore cert
-     * @param type The SSL algorithm to use
+     * @param type             The SSL algorithm to use
      * @return The SSLSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws IOException              If the socket couldn't be created
+     * @throws KeyManagementException   If the KeyManager couldn't be loaded
+     * @throws CertificateException     If the certificate is not correct (null or damaged) or the password is incorrect
      * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
-     * @author gpotter2
+     * @throws KeyStoreException        If your system is not compatible with JKS KeyStore certificates
      */
-    public static SSLSocket getSocketWithCert(String ip, int port, String pathToCert, String passwordFromCert, SecureType type) throws IOException,
-            KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, SocketException{
+    public static SSLSocket getSocketWithCert(
+            String ip, int port, String pathToCert,
+            String passwordFromCert, SecureType type
+    ) throws IOException, KeyManagementException,
+            NoSuchAlgorithmException, CertificateException, KeyStoreException
+    {
         InetAddress ip2 = InetAddress.getByName(ip);
-        if(ip2 == null){
-            new NullPointerException("The ip must be a correct IP !").printStackTrace();
-            return null;
+        if (ip2 == null) {
+            throw new NullPointerException("The ip must be a correct IP !");
         }
+
         File f = new File(pathToCert);
-        if(!f.exists()){
-            new NullPointerException("The specified path point to a non existing file !");
-            return null;
+        if (!f.exists()) {
+            throw new NullPointerException("The specified path point to a non existing file !");
         }
         return getSocketWithCert(ip2, port, new FileInputStream(f), passwordFromCert, type);
     }
@@ -127,25 +92,28 @@ public class SSLSocketKeystoreFactory {
     /**
      * Instantiate sslsocket
      *
-     * @param ip The IP to connect the socket to
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
+     * @param ip               The IP to connect the socket to
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
      * @param passwordFromCert The password of the KeyStore cert
-     * @param type The SSL algorithm to use
+     * @param type             The SSL algorithm to use
      * @return The SSLSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws IOException              If the socket couldn't be created
+     * @throws KeyManagementException   If the KeyManager couldn't be loaded
+     * @throws CertificateException     If the certificate is not correct (null or damaged) or the password is incorrect
      * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws KeyStoreException        If your system is not compatible with JKS KeyStore certificates
      * @author gpotter2
      */
-    public static SSLSocket getSocketWithCert(String ip, int port, InputStream pathToCert, String passwordFromCert, SecureType type) throws IOException,
-            KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, SocketException{
+    public static SSLSocket getSocketWithCert(
+            String ip, int port, InputStream pathToCert,
+            String passwordFromCert, SecureType type
+    ) throws IOException, KeyManagementException,
+            NoSuchAlgorithmException, CertificateException, KeyStoreException
+    {
         InetAddress ip2 = InetAddress.getByName(ip);
-        if(ip2 == null){
-            new NullPointerException("The ip must be a correct IP !").printStackTrace();
-            return null;
+        if (ip2 == null) {
+            throw new NullPointerException("The ip must be a correct IP !");
         }
         return getSocketWithCert(ip2, port, pathToCert, passwordFromCert, type);
     }
@@ -153,25 +121,28 @@ public class SSLSocketKeystoreFactory {
     /**
      * Instantiate sslsocket
      *
-     * @param ip The IP to connect the socket to
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessource()....)
+     * @param ip               The IP to connect the socket to
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessource()....)
      * @param passwordFromCert The password of the KeyStore cert
-     * @param type The SSL algorithm to use
+     * @param type             The SSL algorithm to use
      * @return The SSLSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws IOException              If the socket couldn't be created
+     * @throws KeyManagementException   If the KeyManager couldn't be loaded
+     * @throws CertificateException     If the certificate is not correct (null or damaged) or the password is incorrect
      * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws KeyStoreException        If your system is not compatible with JKS KeyStore certificates
      * @author gpotter2
      */
-    public static SSLSocket getSocketWithCert(InetAddress ip, int port, String pathToCert, String passwordFromCert, SecureType type) throws IOException,
-            KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, SocketException{
+    public static SSLSocket getSocketWithCert(
+            InetAddress ip, int port, String pathToCert,
+            String passwordFromCert, SecureType type
+    ) throws IOException, KeyManagementException,
+            NoSuchAlgorithmException, CertificateException, KeyStoreException
+    {
         File f = new File(pathToCert);
-        if(!f.exists()){
-            new NullPointerException("The specified path point to a non existing file !");
-            return null;
+        if (!f.exists()) {
+            throw new NullPointerException("The specified path point to a non existing file !");
         }
         return getSocketWithCert(ip, port, new FileInputStream(f), passwordFromCert, type);
     }
@@ -179,68 +150,77 @@ public class SSLSocketKeystoreFactory {
     /**
      * Instantiate sslsocket
      *
-     * @param ip The IP to connect the socket to
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
+     * @param ip               The IP to connect the socket to
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
      * @param passwordFromCert The password of the KeyStore cert
-     * @param type The SSL algorithm to use
+     * @param type             The SSL algorithm to use
      * @return The SSLSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws IOException              If the socket couldn't be created
+     * @throws KeyManagementException   If the KeyManager couldn't be loaded
+     * @throws CertificateException     If the certificate is not correct (null or damaged) or the password is incorrect
      * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws KeyStoreException        If your system is not compatible with JKS KeyStore certificates
      * @author gpotter2
      */
-    public static SSLSocket getSocketWithCert(InetAddress ip, int port, InputStream pathToCert, String passwordFromCert, SecureType type) throws IOException,
-            KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, SocketException{
+    public static SSLSocket getSocketWithCert(
+            InetAddress ip, int port, InputStream pathToCert,
+            String passwordFromCert, SecureType type
+    ) throws IOException, KeyManagementException,
+            NoSuchAlgorithmException, CertificateException, KeyStoreException
+    {
         X509TrustManager[] tmm;
-        KeyStore ks  = KeyStore.getInstance(instance);
+        KeyStore ks = KeyStore.getInstance(instance);
+
         ks.load(pathToCert, passwordFromCert.toCharArray());
-        tmm=tm(ks);
+        tmm = tm(ks);
+
         SSLContext ctx = SSLContext.getInstance(type.getType());
         ctx.init(null, tmm, null);
 
-        SSLSocketFactory SocketFactory = (SSLSocketFactory) ctx.getSocketFactory();
-        SSLSocket socket = (SSLSocket) SocketFactory.createSocket(ip, port);
-        return socket;
+        SSLSocketFactory SocketFactory = ctx.getSocketFactory();
+        return (SSLSocket) SocketFactory.createSocket(ip, port);
     }
 
     /**
      * Instantiate sslsocket (beta proxy)
      *
-     * @param ip The IP to connect the socket to
-     * @param port The port of the socket
-     * @param pathToCert The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
+     * @param ip               The IP to connect the socket to
+     * @param port             The port of the socket
+     * @param pathToCert       The path to the KeyStore cert (can be with getClass().getRessourceAsStream()....)
      * @param passwordFromCert The password of the KeyStore cert
-     * @param type The SSL algorithm to use
+     * @param type             The SSL algorithm to use
      * @return The SSLSocket or null if the connection was not possible
-     * @throws IOException If the socket couldn't be created
-     * @throws KeyManagementException  If the KeyManager couldn't be loaded
-     * @throws CertificateException If the certificate is not correct (null or damaged) or the password is incorrect
+     * @throws IOException              If the socket couldn't be created
+     * @throws KeyManagementException   If the KeyManager couldn't be loaded
+     * @throws CertificateException     If the certificate is not correct (null or damaged) or the password is incorrect
      * @throws NoSuchAlgorithmException If the certificate is from an unknown type
-     * @throws KeyStoreException If your system is not compatible with JKS KeyStore certificates
+     * @throws KeyStoreException        If your system is not compatible with JKS KeyStore certificates
      * @author gpotter2
      */
-    public static SSLSocket getSocketWithCert(InetAddress ip, int port, InputStream pathToCert, String passwordFromCert, SecureType type, Proxy proxy) throws IOException,
-            KeyManagementException, NoSuchAlgorithmException, CertificateException, KeyStoreException, SocketException{
+    public static SSLSocket getSocketWithCert(
+            InetAddress ip, int port, InputStream pathToCert,
+            String passwordFromCert, SecureType type, Proxy proxy
+    ) throws IOException, KeyManagementException,
+            NoSuchAlgorithmException, CertificateException, KeyStoreException
+    {
         X509TrustManager[] tmm;
-        KeyStore ks  = KeyStore.getInstance(instance);
+        KeyStore ks = KeyStore.getInstance(instance);
+
         ks.load(pathToCert, passwordFromCert.toCharArray());
-        tmm=tm(ks);
+        tmm = tm(ks);
+
         SSLContext ctx = SSLContext.getInstance(type.getType());
         ctx.init(null, tmm, null);
 
-        SSLSocketFactory SocketFactory = (SSLSocketFactory) ctx.getSocketFactory();
-
+        SSLSocketFactory SocketFactory = ctx.getSocketFactory();
         Socket proxy_s = new Socket(proxy);
-        SSLSocket socket = (SSLSocket) SocketFactory.createSocket(proxy_s, ip.getHostAddress(), port, true);
-        return socket;
+
+        return (SSLSocket) SocketFactory.createSocket(proxy_s, ip.getHostAddress(), port, true);
     }
 
     /**
      * Util class to get the X509TrustManager
-     *
      *
      * @param keystore
      * @return
@@ -251,6 +231,7 @@ public class SSLSocketKeystoreFactory {
     private static X509TrustManager[] tm(KeyStore keystore) throws NoSuchAlgorithmException, KeyStoreException {
         TrustManagerFactory trustMgrFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustMgrFactory.init(keystore);
+
         TrustManager trustManagers[] = trustMgrFactory.getTrustManagers();
         for (int i = 0; i < trustManagers.length; i++) {
             if (trustManagers[i] instanceof X509TrustManager) {
@@ -260,5 +241,5 @@ public class SSLSocketKeystoreFactory {
             }
         }
         return null;
-    };
+    }
 }
